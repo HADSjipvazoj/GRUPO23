ee18a7d086b180a0ab1bd5edaa752c1434a05b99
Xabier Benavides <xacxacxac123@gmail.com>
2019-10-04 23:09:19 +0200

    Validacion del formulario completada

--- js/ValidateFieldsQuestion.js
+++ js/ValidateFieldsQuestion.js
@@ -1,3 +1,66 @@
-function validate() {
-    
+$(document).ready(function () {
+    $("#formulario").submit(function () {
+      if(!check_fields()) return false;
+      if(!check_email()) return false;
+      if(!check_question()) return false;
+      if(!check_difficulty()) return false;
+      $("#aviso").html("");
+      return true;
+    });
+    $("#reset").click( function(){
+      $("#user1").prop("checked", false);
+      $("#user2").prop("checked", false);
+      $("#email").val("");
+      $("#enunciado").val("");
+      $("#correcta").val("");
+      $("#incorrecta1").val("");
+      $("#incorrecta2").val("");
+      $("#incorrecta3").val("");
+      $("#dificultad").val("1");
+      $("#tema").val("");
+      $("#aviso").html("");
+    });
+});
+
+function check_fields(){
+  if(!$("#user1").is(":checked") && !$("#user2").is(":checked")){ $("#aviso").html("Selecciona un tipo de usuario."); ;return false;}
+  if($("#email").val().length == 0){ $("#aviso").html("Indica tu email."); return false;}
+  if($("#enunciado").val().length == 0){ $("#aviso").html("Indica un enunciado."); return false;}
+  if($("#correcta").val().length == 0){ $("#aviso").html("Indica la respuesta correcta."); return false;}
+  if($("#incorrecta1").val().length == 0){ $("#aviso").html("Indica las respuestas incorrectas."); return false;}
+  if($("#incorrecta2").val().length == 0){ $("#aviso").html("Indica las respuestas incorrectas."); return false;}
+  if($("#incorrecta3").val().length == 0){ $("#aviso").html("Indica las respuestas incorrectas."); return false;}
+  if($("#dificultad").val().length == 0){ $("#aviso").html("Selecciona un nivel de dificultad."); return false;}
+  if($("#tema").val().length == 0){ $("#aviso").html("Indica un tema para la pregunta.");return false;}
+  return true;
+}
+
+function check_email(){
+  var pattern;
+  if($("#user1").is(":checked")){
+    pattern = /^([a-z]|[A-Z])+[0-9]{3}@ikasle.ehu.(es|eus)$/;
+  }else{
+    pattern = /^([a-z]|[A-Z])+(.([a-z]|[A-Z])+)?@ehu.(es|eus)$/;
+  }
+  if(pattern.test($("#email").val())){
+    return true;
+  }else{
+    $("#aviso").html("Email incorrecto para el tipo de usuario indicado.");
+    return false;
+  }
+}
+
+function check_question(){
+  if($("#enunciado").val().length < 10){ $("#aviso").html("El enunciado es demasiado corto."); return false;}
+  return true;
+}
+
+function check_difficulty(){
+  var pattern = /^(1|2|3)$/;
+  if(pattern.test($("#dificultad").val())){
+    return true;
+  }else {
+    $("#aviso").html("Nivel de dificultad incorrecto, elige un numero del 1 al 3.");
+    return false;
+  }
 }
--- php/QuestionForm.php
+++ php/QuestionForm.php
@@ -2,27 +2,34 @@
 <html>
     <head>
         <?php include '../html/Head.html'?>
+        <script src="../js/jquery-3.4.1.min.js"></script>
+        <script src="../js/ValidateFieldsQuestion.js"></script>
     </head>
     <body>
         <?php include '../php/Menus.php' ?>
         <section class="main" id="s1">
             <div>
-                <form action = "" name = "formulario" id = "formulario" onsubmit = "return validate();">
+                <form action = "" name = "formulario" id = "formulario">
                     Marque el tipo de usuario <br>
-                    <input type="radio" name="user" id="user" value="alumno" checked>Alumno<br>
-                    <input type="radio" name="user" id="user" value="profesor">Profesor<br>
-                    Dirección de correo: <input type="text" id= "email" name="email"><br>
-                    Enunciado de la pregunta: <input type="text" id= "enunciado" name="enunciado"><br>
+                    <input type="radio" name="user" id="user1" value="alumno">Alumno<br>
+                    <input type="radio" name="user" id="user2" value="profesor">Profesor<br><br>
+                    Dirección de correo: <input type="text" id= "email" name="email"><br><br>
+                    Enunciado de la pregunta: <input type="text" id= "enunciado" name="enunciado"><br><br>
                     Respuesta Correcta: <input type="text" id= "correcta" name="correcta"><br>
                     Respuesta Incorrecta 1: <input type="text" id= "incorrecta1" name="incorrecta1"><br>
                     Respuesta Incorrecta 2: <input type="text" id= "incorrecta2" name="incorrecta2"><br>
-                    Respuesta Incorrecta 3: <input type="text" id= "incorrecta3" name="incorrecta3"><br>
-                    <input type="radio" name="user" id="user" value="alumno" checked> Facil 
-                    <input type="radio" name="user" id="user" value="profesor"> Medio 
-                    <input type="radio" name="user" id="user" value="alumno"> Dificil <br>
-                    Tema de la pregunta: <input type="text" id= "tema" name="tema"><br>
-                    <button type="submit">Enviar</button>
-                    <button type="reset">Reiniciar</button>
+                    Respuesta Incorrecta 3: <input type="text" id= "incorrecta3" name="incorrecta3"><br><br>
+                    Nivel de dificultad:
+                    <select name="dificultad" id="dificultad">
+                        <option value="1"> 1 </option>
+                        <option value="2"> 2 </option>
+                        <option value="3"> 3 </option>
+                    </select> <br><br>
+                    Tema de la pregunta: <input type="text" id= "tema" name="tema"><br><br>
+                    <input id="submit" type="submit" value="Enviar">
+                    <input id="reset" type="button" value="Deshacer">
+                    <br><br>
+                    <div id="aviso"></div>
                 </form>
             </div>
         </section>
