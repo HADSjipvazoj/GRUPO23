8376574e3b543acc2ca39371d049b6a56e95590d
Xabier Benavides <xacxacxac123@gmail.com>
2019-10-15 13:02:40 +0200

    Falta comentar

--- php/AddQuestionWithImage.php
+++ php/AddQuestionWithImage.php
@@ -8,48 +8,45 @@
         <section class="main" id="s1">
             <div>
                 <?php
-                    // Suprimir los warnings en caso de errores.
                     error_reporting(E_ERROR | E_PARSE);
                     $user = "root";
                     $password = "";
-                    $db = "Quiz";
+                    $db = "quiz";
 
                     if (!$data_base = mysqli_connect("localhost", $user, $password, $db))
                     {
                         die("No ha sido posible establecer la conexión con el servidor. <br> Inténtelo de nuevo más adelante.");
-                        //die ("Fallo al conectar a MySQL: " . mysqli_connect_error());
                     }else{
-                        echo("Conexión con el servidor establecida establecida. <br>");
+                        echo("Conexión con el servidor establecida. <br>");
                     }
 
                     $image = addslashes(file_get_contents($_FILES["fileupload"]["tmp_name"]));
-                    $insert =  "INSERT INTO Preguntas (tipo_usuario, 
-                                                      correo, 
-                                                      enunciado, 
-                                                      r_correcta, 
-                                                      r_incorrecta1, 
-                                                      r_incorrecta2, 
-                                                      r_incorrecta3,  
-                                                      dificultad, 
+                    $insert =  "INSERT INTO preguntas (tipo_usuario,
+                                                      correo,
+                                                      enunciado,
+                                                      r_correcta,
+                                                      r_incorrecta1,
+                                                      r_incorrecta2,
+                                                      r_incorrecta3,
+                                                      dificultad,
                                                       tema,
-                                                      imagen)  
-                                VALUES ('".$_REQUEST["user"]."',
-                                        '".$_REQUEST["email"]."',
-                                        '".$_REQUEST["enunciado"]."',
+                                                      imagen)
+                                VALUES ('".$_REQUEST['user']."',
+                                        '".$_REQUEST['email']."',
+                                        '".$_REQUEST['enunciado']."',
                                         '".$_REQUEST['correcta']."',
                                         '".$_REQUEST['incorrecta1']."',
                                         '".$_REQUEST['incorrecta2']."',
-                                        '".$_REQUEST['incorrecta3']."', 
+                                        '".$_REQUEST['incorrecta3']."',
                                         '".$_REQUEST['dificultad']."',
                                         '".$_REQUEST['tema']."',
                                         '$image')";
-                    
+
                     if ($data_base->query($insert) == TRUE) {
-                        echo "Nueva pregunta almacenada con éxito. <br>";
+                        echo("Nueva pregunta almacenada con éxito. <br>");
                         echo("Puede consultar las preguntas en el siguiente <a href = 'ShowQuestionsWithImage.php'>enlace</a>.");
                     } else {
-                        echo("No ha sido posible insertar su pregunta en la base de datos.");
-                        //echo "Error: <br>" . $data_base->error;
+                        echo("No ha sido posible insertar su pregunta en la base de datos, inténtelo de nuevo más tarde.");
                     }
 
                     $data_base->close();
--- php/ShowQuestionsWithImage.php
+++ php/ShowQuestionsWithImage.php
@@ -9,26 +9,23 @@
         <section class="main" id="s1">
             <div>
             <?php
-                // Suprimir los warnings en caso de errores.
                 error_reporting(E_ERROR | E_PARSE);
                 $user = "root";
                 $password = "";
-                $db = "Quiz";
+                $db = "quiz";
 
                 $data_base = mysqli_connect("localhost",$user, $password, $db);
-                
+
                 if (!$data_base)
                 {
                     die("No ha sido posible conectarse a la base de datos.<br> Por favor, inténtelo más adelante.");
-                    //die ("Fallo al conectar a MySQL: " . mysqli_connect_error());
                 }
 
-                $query = "SELECT correo, enunciado, r_correcta, imagen FROM Preguntas ORDER BY id";
+                $query = "SELECT correo, enunciado, r_correcta, imagen FROM preguntas ORDER BY id";
                 if(!$results = $data_base->query($query)){
                     die("No ha sido posible acceder a las preguntas. <br> Por favor, inténtelo más adelante.");
-                    //die ("Fallo al hacer la query a MySQL: " . mysqli_error($data_base));
                 }
-                
+
                 echo("<h1>Preguntas almacenadas en la Base de Datos</h1><br><br>");
                 echo("<table  class='center' id = 'tabla_bd'>");
                 echo("<tr> <th>Dirección de correo</th> <th>Enunciado</th> <th>Respuesta Correcta</th> <th>Imagen</th> </tr>");
