df10368b6e2eb2235ff0fe398eac4647dd328d58
David <sandstrom.david98@gmail.com>
2019-10-22 12:55:33 +0200

    Ventana de LogOut

---
+++ php/LogOut.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <?php include '../html/Head.html'?>
+        <script src="../js/jquery-3.4.1.min.js"></script>
+        <link rel="stylesheet" type="text/css" href="../styles/personal.css">
+    </head>
+    <body>
+        <?php include '../php/Menus.php' ?>
+        <section class="main" id="s1">
+            <div>
+                <h1>Adios</h1>
+            </div>
+        </section>
+        <?php include '../html/Footer.html' ?>
+    </body>
+</html>
